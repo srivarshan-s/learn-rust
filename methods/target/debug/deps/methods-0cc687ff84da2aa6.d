b992ba71816330514af6ece0ff643b898b9f7bc5
Srivarshan <srivarshan02@gmail.com>
2021-11-24 23:56:53 +0530

    Create methods

---
+++ methods/target/debug/deps/methods-0cc687ff84da2aa6.d
@@ -0,0 +1,5 @@
+/home/sri/Documents/learn-rust/methods/target/debug/deps/methods-0cc687ff84da2aa6: src/main.rs
+
+/home/sri/Documents/learn-rust/methods/target/debug/deps/methods-0cc687ff84da2aa6.d: src/main.rs
+
+src/main.rs:
---
+++ methods/target/debug/methods.d
@@ -0,0 +1,1 @@
+/home/sri/Documents/learn-rust/methods/target/debug/methods: /home/sri/Documents/learn-rust/methods/src/main.rs
