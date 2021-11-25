0beb6c3db4da3bd0803773acf9e39b814c937c17
Srivarshan <srivarshan02@gmail.com>
2021-11-25 09:06:51 +0530

    Create enums files

---
+++ enums/target/debug/deps/enums-caa260f0711d65f4.d
@@ -0,0 +1,5 @@
+/home/sri/Documents/learn-rust/enums/target/debug/deps/enums-caa260f0711d65f4: src/main.rs
+
+/home/sri/Documents/learn-rust/enums/target/debug/deps/enums-caa260f0711d65f4.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/home/sri/Documents/learn-rust/enums/target/debug/enums: /home/sri/Documents/learn-rust/enums/src/main.rs
