94304e8ac204a6800eda3cd9a529e95fb022c049
Srivarshan <srivarshan02@gmail.com>
2021-11-25 10:37:17 +0530

    Update enums files

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+// Defining an Enum
+
+enum IpAddrKind {
+    V4,
+    V6,
 }
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    // Enum values
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    enum IpAddrEnum {
+        V4(String),
+        V6(String),
+    }
+    let home = IpAddrEnum::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrEnum::V6(String::from("::1"));
+
+    enum IpAddrU8 {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+    let home = IpAddrU8::V4(127, 0, 0, 1);
+    let loopback = IpAddrU8::V6(String::from("::1"));
+
+    struct IpV4Addr {}
+    struct IpV6Addr {}
+    enum IpAddrStruct {
+        V4(IpV4Addr),
+        V6(IpV6Addr),
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    impl Message {
+        fn call(&self) {
+            println!("Function call() called");
+        }
+    }
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
+fn route(ip_kind: IpAddrKind) {}
---
+++ enums/target/rls/debug/deps/enums-c3058140020b3129.d
@@ -0,0 +1,5 @@
+/home/sri/Documents/learn-rust/enums/target/rls/debug/deps/enums-c3058140020b3129.rmeta: src/main.rs
+
+/home/sri/Documents/learn-rust/enums/target/rls/debug/deps/enums-c3058140020b3129.d: src/main.rs
+
+src/main.rs:
