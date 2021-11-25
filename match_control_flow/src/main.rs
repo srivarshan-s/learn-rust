6975403459990842ff94326d19bf87a6ea3f3d83
Srivarshan <srivarshan02@gmail.com>
2021-11-25 15:35:17 +0530

    Update match files

--- match_control_flow/src/main.rs
+++ match_control_flow/src/main.rs
@@ -1,3 +1,40 @@
+// The match control flow operator
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let coin1 = Coin::Penny;
+    let coin2 = Coin::Nickel;
+    let coin3 = Coin::Dime;
+    let coin4 = Coin::Quarter(UsState::Alaska);
+    println!("{}", value_in_cents(&coin1));
+    println!("{}", value_in_cents(&coin2));
+    println!("{}", value_in_cents(&coin3));
+    println!("{}", value_in_cents(&coin4));
 }
---
+++ match_control_flow/target/rls/debug/deps/match_control_flow-0ba0c0958cfcfe4b.d
@@ -0,0 +1,5 @@
+/home/sri/Documents/learn-rust/match_control_flow/target/rls/debug/deps/match_control_flow-0ba0c0958cfcfe4b.rmeta: src/main.rs
+
+/home/sri/Documents/learn-rust/match_control_flow/target/rls/debug/deps/match_control_flow-0ba0c0958cfcfe4b.d: src/main.rs
+
+src/main.rs:
